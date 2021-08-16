4944c50a91691568a2a83292755ca99f8c5de3ba
Heitor <heitorwerneck@hotmail.com>
2021-08-15 21:57:28 -0300

    scene data

---
+++ Assets/MyPackages/flashlight/Flashlight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public AudioClip soundOn;
+	public AudioClip soundOff;
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			if(GetComponentInChildren<Light>().enabled == false)
+			{
+				GetComponentInChildren<Light>().enabled = true;
+				GetComponent<AudioSource>().clip = soundOn;
+				GetComponent<AudioSource>().Play();
+			}
+			else
+			{
+				GetComponentInChildren<Light>().enabled = false;
+				GetComponent<AudioSource>().clip = soundOff;
+				GetComponent<AudioSource>().Play();
+			}
+		}
+	}
+}
